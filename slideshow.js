be5359f2bc8f26f90076342715e50a8168d9fc13
dinesh0831 <81325951+dinesh0831@users.noreply.github.com>
2021-08-12 11:31:14 +0530

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,62 @@
+const image=["image\\image0.jpg","image\\image1.jpg","image\\image2.jpg","image\\image3.jpg","image\\image4.jpg","image\\image5.jpg",]
+let index=0;
+function img () 
+{
+    if(index==image.length)
+    {
+        index=0;
+    }
+    const imag=document.querySelector(".img");
+    imag.setAttribute("src",`${image[index]}`)
+    index++; 
+}  
+
+var interval=setInterval(img,1000*2)
+function restart()
+{
+     interval=setInterval(img,1000*2)
+}
+
+
+    function stop()
+    {
+        clearInterval(interval);   
+    }
+  
+   
+    
+
+function backward()
+{
+   if(index==0)
+    {
+        index=image.length-1;
+    }
+    const imag=document.querySelector(".img");
+    imag.setAttribute("src",`${image[index]}`)
+    index--; 
+    
+    
+
+}
+function forward()
+{
+    if(index==image.length)
+    {
+        index=0;
+    }
+    const imag=document.querySelector(".img");
+    imag.setAttribute("src",`${image[index]}`)
+    index++;
+    
+}
+
+
+
+
+
+
+
+
+
+
